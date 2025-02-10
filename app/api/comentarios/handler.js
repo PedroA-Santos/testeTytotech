0de4c62d3ab7fa2cf3c7443cdf368689ca3195f1
Pedro Antonio <eupedroassuncao@gmail.com>
2025-02-10 10:16:52 -0300

    trabalhando no crud de comentarios

---
+++ app/api/comentarios/handler.js
@@ -0,0 +1,65 @@
+import { connection } from "../database/db";
+
+
+export const getComentarios = async () => {
+    try {
+        const [results] = await connection.query(`SELECT * FROM comentarios`)
+        return results;
+    } catch (error) {
+        console.error("Erro ao buscar comentários no banco", error)
+        throw new Error("Erro ao buscar comentários")
+    }
+}
+
+
+
+export const getComentariosById = async (id) => {
+    try {
+        const [results] = await connection.query(`SELECT * FROM comentarios WHERE id = ?`, [id])
+        return results;
+    } catch (error) {
+        console.error("Erro ao buscar comentario pelo id no banco", error)
+        throw new Error("Erro ao buscar comentário")
+    }
+}
+
+
+
+
+export const postComentarios = async (post_id, usuario_id, conteudo, criado_em) => {
+
+    try {
+        const results = await connection.query(`INSERT INTO comentarios (post_id, usuario_id, conteudo, criado_em) VALUES (?,?,?,?)`, [post_id, usuario_id, conteudo, criado_em])
+        return results;
+    } catch (error) {
+        console.error("Erro ao inserir dados na tabela do banco")
+        throw new Error("Erro ao criar comentario")
+    }
+
+}
+
+
+
+export const putComentarios = async (post_id, usuario_id, conteudo, criado_em, id) => {
+    try {
+        const results = await connection.query(`UPDATE comentarios SET post_id = ? , usuario_id = ? , conteudo = ?, criado_em = ? WHERE id =? `, [post_id, usuario_id, conteudo, criado_em, id]);
+        return results;
+    } catch (error) {
+        console.error("Erro ao atualizar comentario no banco", error)
+        throw new Error("Erro ao atualizar comentario")
+    }
+}
+
+
+
+
+export const deleteComentarios = async (id) => {
+    try {
+        const results = await connection.query(`DELETE FROM comentarios WHERE id = ?`, [id]);
+        return results;
+    } catch (error) {
+        console.error("Erro ao deletar no banco", error)
+        throw new Error("Erro ao deletar comentário")
+
+    }
+}
