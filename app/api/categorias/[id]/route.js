1075449548d124994eddcf7da54922bed6f47d90
Pedro Antonio <eupedroassuncao@gmail.com>
2025-02-10 10:31:38 -0300

    arrumando importação

--- app/api/categorias/[id]/route.js
+++ app/api/categorias/[id]/route.js
@@ -1,4 +1,4 @@
-import { deleteCategoria, getCategoriasById, putCategoria } from "../../handler";
+import { deleteCategoria, getCategoriasById, putCategoria } from "../handler";
 import { NextResponse } from "next/server";
 
 export async function GET(request, { params }) {
