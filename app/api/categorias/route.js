75dc039c6020c081465e026a7c9dbf28770a9ed8
Pedro Antonio <eupedroassuncao@gmail.com>
2025-02-10 10:28:39 -0300

    Arrumando a estrutração e rotas

--- app/api/categorias/route.js
+++ app/api/categorias/route.js
@@ -1,20 +1,10 @@
 import { NextResponse } from "next/server";
-import { getCategorias, getCategoriasById, postCategoria, putCategoria, deleteCategoria } from "./handler";
+import { getCategorias, postCategoria } from "./handler";
 
-export async function GET(request) {
+export async function GET() {
     try {
-        const { searchParams } = new URL(request.url);
-        const id = searchParams.get("id");
-
-        if (id) {
-            const categoria = await getCategoriasById(id);
-            if (!categoria) {
-                return NextResponse.json({ message: "Categoria não encontrada" }, { status: 404 });
-            }
-            return NextResponse.json(categoria);
-        }
-
         const categorias = await getCategorias();
+
         if (categorias.length === 0) {
             return NextResponse.json({ message: "Nenhuma categoria encontrada" }, { status: 404 });
         }
@@ -39,41 +29,5 @@ export async function POST(request) {
     } catch (error) {
         console.error("Erro na rota POST", error.message);
         return NextResponse.json({ error: error.message || "Erro ao criar categoria" }, { status: 500 });
-    }
-}
-
-export async function PUT(request) {
-    try {
-        const { searchParams } = new URL(request.url);
-        const id = searchParams.get("id");
-
-        if (!id) {
-            return NextResponse.json({ error: "ID obrigatório para atualização" }, { status: 400 });
-        }
-
-        const body = await request.json();
-        const categoriaAtualizada = await putCategoria(id, body);
-
-        return NextResponse.json(categoriaAtualizada, { status: 200 });
-    } catch (error) {
-        console.error("Erro na rota PUT", error.message);
-        return NextResponse.json({ error: error.message || "Erro ao atualizar categoria" }, { status: 500 });
-    }
-}
-
-export async function DELETE(request) {
-    try {
-        const { searchParams } = new URL(request.url);
-        const id = searchParams.get("id");
-
-        if (!id) {
-            return NextResponse.json({ error: "ID obrigatório para deleção" }, { status: 400 });
-        }
-
-        await deleteCategoria(id);
-        return NextResponse.json({ message: "Categoria deletada com sucesso" }, { status: 200 });
-    } catch (error) {
-        console.error("Erro na rota DELETE", error.message);
-        return NextResponse.json({ error: error.message || "Erro ao deletar categoria" }, { status: 500 });
     }
 }
