1660176e92163ddc07dca76730e666a303047370
Pedro Antonio <eupedroassuncao@gmail.com>
2025-02-12 20:39:52 -0300

    Adicionando login de usuário simples, falta adicionar hash e bcrypt

--- app/api/usuarios/handler.js
+++ app/api/usuarios/handler.js
@@ -1,4 +1,5 @@
 import { connection } from "../database/db"
+import { bcrypt } from "bcryptjs"
 
 
 export const getUsuarios = async () => {
@@ -17,7 +18,7 @@ export const getUsuarios = async () => {
 export const getUsuariosById = async (id) => {
 
     try {
-        const [results] = await connection.query(`SELECT * FROM usuarios WHERE id =?`);
+        const [results] = await connection.query(`SELECT * FROM usuarios WHERE id =?`, [id]);
         return results;
     } catch (error) {
         console.error("Erro ao listar os usuario no banco", error);
@@ -64,4 +65,32 @@ export const deleteUsuario = async (id) => {
         console.error("Erro ao deletar usuário no banco", error)
         throw new Error("Erro ao deltar usuário")
     }
+}
+
+
+export const getUsuarioByemail = async (email) => {
+    try {
+        const [results] = await connection.query(`SELECT * FROM usuarios WHERE email = ?`, [email]);
+
+        // Se não encontrar nenhum usuário, retorna null
+        if (results.length === 0) {
+            return null;
+        }
+
+        // Retorna o primeiro usuário encontrado
+        return results[0];
+    } catch (error) {
+        console.error("Erro ao buscar email", error);
+        throw new Error("Erro ao buscar usuário");
+    }
+}
+
+
+
+
+export async function criarUsuario(nome, email, senha) {
+
+    const senhaHash = await bcrypt.hash(senha, 10); // Hash da senha
+
+    await db.query("INSERT INTO usuarios (nome, email, senha) VALUES (?, ?, ?)", [nome, email, senhaHash]);
 }
