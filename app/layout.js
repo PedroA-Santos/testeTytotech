b670d1e3a32c39f433ae6b3131cd178b229db843
Pedro Antonio <eupedroassuncao@gmail.com>
2025-02-19 21:35:20 -0300

    Melhorando o filter da categoria

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -23,7 +22,6 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-
         {children}
       </body>
     </html>
